e82d4fc0934b71afad91438762013d2b329652a9
aletowk <None>
2024-12-29 17:18:23 +0100

    Add simple test of adding CityUI.tsn assets in a GridContainer

--- scripts/civ_panel/civ_panel.gd
+++ scripts/civ_panel/civ_panel.gd
@@ -1,13 +1,29 @@
 extends CanvasLayer
 
-signal start_game
+var city_node = preload("res://scenes/assets/CityUI.tscn")
+
+var cities = []
+
+func update_desc():
+	var msg = ""
+	for city in cities:
+		msg += city + "\n"
+	$CivDescription.text = msg
 
 func _ready() -> void:
-	$Start.connect("pressed", self.start)
-	
-func start() -> void:
-	self.show_message("Started !")
+	$Add.connect("pressed", self.add_city)
 
-func show_message(text):
-	$CityDescription.text = text
-	$CityDescription.show()
+	self.add_city()
+	self.add_city()
+
+func add_city() -> void:
+	var new_city_name = "City" + str(len(cities) + 1)
+	
+	var new_city = city_node.instantiate()
+	var city_lbl: Label = new_city.get_node("Label")
+	city_lbl.text = new_city_name
+	$GridContainer.add_child(new_city)
+	$GridContainer.set("theme_override_constants/v_separation", city_lbl.size.x/2)
+	
+	print(new_city_name)
+	cities.append(new_city_name)
